19c914a24025b354ce871abd57c365b874fe6698
Wan D Mean <ph.mean@gmail.com>
2021-06-09 18:50:27 +0700

    add unit test topic service deleteTopic

--- src/topics/services/topic.service.spec.ts
+++ src/topics/services/topic.service.spec.ts
@@ -5,83 +5,111 @@ import { TopicRepository } from '../repositories/topic.repository'
 import { TopicService } from './topic.service'
 
 describe('TopicService', () => {
-    let topicService: TopicService
-    let postService: PostService
+  let topicService: TopicService
+  let postService: PostService
 
-    beforeAll(async () => {
-        const moduleRef = await Test.createTestingModule({
-            providers: [
-                TopicService,
-                TopicRepository,
-                { provide: PostService, useValue: {} },
-            ],
-        }).compile()
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        TopicService,
+        TopicRepository,
+        { provide: PostService, useValue: {} },
+      ],
+    }).compile()
 
-        topicService = moduleRef.get(TopicService)
-        postService = moduleRef.get(PostService)
+    topicService = moduleRef.get(TopicService)
+    postService = moduleRef.get(PostService)
+  })
+
+  describe('getTopics', () => {
+    it('should be success', async () => {
+      const mockTopics = [
+        mockTopic({ id: 11, forumId: 22, userId: 33, name: 'topic10' }),
+        mockTopic({ id: 111, forumId: 222, userId: 333, name: 'topic10' }),
+        mockTopic({ id: 1111, forumId: 2222, userId: 3333, name: 'topic10' }),
+      ]
+      TopicRepository.prototype.find = jest.fn().mockReturnValue(mockTopics)
+      const result = await topicService.getTopics()
+      expect(result).toEqual([
+        { id: 11, forumId: 22, userId: 33, name: 'topic10' },
+        { id: 111, forumId: 222, userId: 333, name: 'topic10' },
+        { id: 1111, forumId: 2222, userId: 3333, name: 'topic10' },
+      ])
     })
 
-    describe('getTopics', () => {
-        it('should be success', async () => {
-            const mockTopics = [
-                mockTopic({ id: 11, forumId: 22, userId: 33, name: 'topic10' }),
-                mockTopic({ id: 111, forumId: 222, userId: 333, name: 'topic10' }),
-                mockTopic({ id: 1111, forumId: 2222, userId: 3333, name: 'topic10' }),
-            ]
-            TopicRepository.prototype.find = jest.fn().mockReturnValue(mockTopics)
-            const result = await topicService.getTopics()
-            expect(result).toEqual([
-                { id: 11, forumId: 22, userId: 33, name: 'topic10' },
-                { id: 111, forumId: 222, userId: 333, name: 'topic10' },
-                { id: 1111, forumId: 2222, userId: 3333, name: 'topic10' }
-            ])
-        })
+    it('should be throw error Topics not found', async () => {
+      TopicRepository.prototype.find = jest.fn().mockReturnValue([])
+      await expect(topicService.getTopics()).rejects.toThrow('Topics not found')
+    })
+  })
 
-
-        it('should be throw error Topics not found', async () => {
-            TopicRepository.prototype.find = jest.fn().mockReturnValue([])
-            await expect(topicService.getTopics()).rejects.toThrow('Topics not found')
-        })
+  describe('getTopic', () => {
+    it('should be success', async () => {
+      TopicRepository.prototype.findOne = jest
+        .fn()
+        .mockReturnValue(
+          mockTopic({ id: 11, forumId: 22, userId: 33, name: 'topic10' }),
+        )
+      const result = await topicService.getTopic(11)
+      expect(result).toEqual({
+        id: 11,
+        forumId: 22,
+        userId: 33,
+        name: 'topic10',
+      })
     })
 
-    describe('getTopic', () => {
-        it('should be success', async () => {
-            TopicRepository.prototype.findOne = jest.fn().mockReturnValue(mockTopic({ id: 11, forumId: 22, userId: 33, name: 'topic10' }))
-            const result = await topicService.getTopic(11)
-            expect(result).toEqual({ id: 11, forumId: 22, userId: 33, name: 'topic10' })
-        })
-
-        it('should be throw error Topic not found', async () => {
-            TopicRepository.prototype.findOne = jest.fn().mockReturnValue(false)
-            await expect(topicService.getTopic(1)).rejects.toThrow('Topic not found')
-
-        })
+    it('should be throw error Topic not found', async () => {
+      TopicRepository.prototype.findOne = jest.fn().mockReturnValue(false)
+      await expect(topicService.getTopic(1)).rejects.toThrow('Topic not found')
     })
+  })
 
-    describe('getTopicsByForumId', () => {
-        it('should be throw error Topics not found', async () => {
-            TopicRepository.prototype.find = jest.fn().mockReturnValue([])
-            await expect(topicService.getTopicsByForumId(1)).rejects.toThrow('Topics not found')
-
-        })
-        it('should be success', async () => {
-            const mockTopics = [
-                mockTopic({ id: 10, forumId: 20, userId: 3, name: 'topic10' }),
-            ]
-            TopicRepository.prototype.find = jest.fn().mockReturnValue(mockTopics)
-            const result = await topicService.getTopicsByForumId(20)
-            expect(result).toEqual([{ id: 10, forumId: 20, userId: 3, name: 'topic10' }])
-
-        })
+  describe('getTopicsByForumId', () => {
+    it('should be throw error Topics not found', async () => {
+      TopicRepository.prototype.find = jest.fn().mockReturnValue([])
+      await expect(topicService.getTopicsByForumId(1)).rejects.toThrow(
+        'Topics not found',
+      )
     })
-
-    describe('getTopic', () => {
-        it('should be remove createdAt and updatedAt property', async () => {
-
-        })
-
-        it('should be throw error Topic not found', async () => {
-
-        })
+    it('should be success', async () => {
+      const mockTopics = [
+        mockTopic({ id: 10, forumId: 20, userId: 3, name: 'topic10' }),
+      ]
+      TopicRepository.prototype.find = jest.fn().mockReturnValue(mockTopics)
+      const result = await topicService.getTopicsByForumId(20)
+      expect(result).toEqual([
+        { id: 10, forumId: 20, userId: 3, name: 'topic10' },
+      ])
     })
+  })
+
+  describe('deleteTopic', () => {
+    it('should be throw error Topic not found', async () => {
+      TopicRepository.prototype.findOne = jest.fn()
+      await expect(
+        topicService.deleteTopic({ topicId: 1234, userId: 3456 })
+      ).rejects.toThrow('Topic not found')
+    })
+    it('should be throw error User not owner', async () => {
+      TopicRepository.prototype.findOne = jest.fn().mockReturnValue(
+        mockTopic({ userId: 1234 })
+      )
+      await expect(
+        topicService.deleteTopic({ topicId: 1234, userId: 3456 })
+      ).rejects.toThrow('User not owner')
+    })
+    it('should be deleted success', async () => {
+      TopicRepository.prototype.findOne = jest.fn().mockReturnValue(
+        mockTopic({ userId: 1234 })
+      )
+      postService.deletePosts = jest.fn()
+      TopicRepository.prototype.delete = jest.fn()
+      await topicService.deleteTopic({ userId: 1234, topicId: 6543 })
+      expect(postService.deletePosts).toBeCalledTimes(1)
+      expect(postService.deletePosts).toBeCalledWith(6543)
+      expect(TopicRepository.prototype.delete).toBeCalledWith({ id: 6543 })
+    })
+  })
+
 })
--- src/topics/services/topic.service.ts
+++ src/topics/services/topic.service.ts
@@ -22,13 +22,13 @@ export class TopicService {
 
   async getTopics(): Promise<TTopic[]> {
     const topics = await this.topicRepo.find({ order: { createdAt: 'ASC' } })
-    if(topics.length === 0)throw new NotFoundException('Topics not found')
+    if (topics.length === 0) throw new NotFoundException('Topics not found')
     return plainToClass(TTopic, topics)
   }
 
   async getTopic(id: number): Promise<TTopic> {
     const topic = await this.topicRepo.findOne(id)
-    if(!topic)throw new NotFoundException('Topic not found')
+    if (!topic) throw new NotFoundException('Topic not found')
     return plainToClass(TTopic, topic)
   }
 
@@ -37,7 +37,7 @@ export class TopicService {
       where: { forumId },
       order: { createdAt: 'ASC' },
     })
-    if(topics.length === 0)throw new NotFoundException('Topics not found')
+    if (topics.length === 0) throw new NotFoundException('Topics not found')
     return plainToClass(TTopic, topics)
   }
 
@@ -58,7 +58,8 @@ export class TopicService {
   async deleteTopic(data: IDeleteTopic): Promise<DeleteResult> {
     const topicData = await this.topicRepo.findOne({ id: data.topicId })
     if (!topicData) throw new NotFoundException('Topic not found')
-    if (topicData.userId !== data.userId) throw new NotAcceptableException('User not owner')
+    if (topicData.userId !== data.userId)
+      throw new NotAcceptableException('User not owner')
     await this.postService.deletePosts(data.topicId)
     const deletedTopic = await this.topicRepo.delete({ id: data.topicId })
     return deletedTopic
@@ -67,7 +68,8 @@ export class TopicService {
   async editTopic(data: IEditTopic): Promise<Topic> {
     const topicData = await this.topicRepo.findOne({ id: data.topicId })
     if (!topicData) throw new NotFoundException('Topic not found')
-    if (topicData.userId !== data.userId) throw new NotAcceptableException('User not owner')
+    if (topicData.userId !== data.userId)
+      throw new NotAcceptableException('User not owner')
     const editedTopic = await this.topicRepo.save({
       id: data.topicId,
       name: data.name,
